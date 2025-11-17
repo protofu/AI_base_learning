4c04b8d527b114968966fecf20ac1c4574e21015
protofu <protofu@vivans.net>
2025-11-17 11:24:40 +0900

    실습 3 - 손실함수(+MAE)

--- week1/day1/prac03.py
+++ week1/day1/prac03.py
@@ -6,3 +6,13 @@ y_pred = np.array([1.1, 1.9, 3.2])
 
 mse = np.mean((y_true - y_pred) ** 2)
 print("MSE:", mse)
+
+x = np.linspace(-2, 2, 100)
+mse = x**2
+mae = np.abs(x)
+
+plt.plot(x, mse, label="MSE (x^2)")
+plt.plot(x, mae, label="MAE (|x|)")
+plt.legend()
+plt.title("MSE vs MAE 손실함수 형태")
+plt.show()
